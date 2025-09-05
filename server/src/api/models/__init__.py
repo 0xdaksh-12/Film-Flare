3ebd04bcf1ecbbcd48d3bdce63cfbd8c5f8716f7
0xdaksh-12 <0xdaksh.12@gmail.com>
2025-09-05 18:03:19 +0530

    feat: define data models in __init__.py

---
+++ server/src/api/models/__init__.py
@@ -0,0 +1,14 @@
+from .model import (
+    Movie,
+    Genre,
+    Actor,
+    Director,
+    MovieGenreLink,
+    UserRating,
+    User,
+    Session,
+    UserAuth,
+    Year,
+    MovieData,
+    UserRating,
+)
