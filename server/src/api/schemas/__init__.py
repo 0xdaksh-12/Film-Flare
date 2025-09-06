6bd544a566c6e5b517229c6499c747bfec749dcc
0xdaksh-12 <0xdaksh.12@gmail.com>
2025-09-06 17:06:05 +0530

    feat: define schemas for __init__

---
+++ server/src/api/schemas/__init__.py
@@ -0,0 +1,19 @@
+from .auth import (
+    AuthGuard,
+    AuthResponse,
+    LoginData,
+    LoginRequest,
+    RegisterData,
+    RegisterRequest,
+    UserMe,
+    AuthProvider,
+    ClientMeta,
+    payloadToken,
+)
+from .movie import (
+    Movie,
+    MovieDetail,
+    MovieRatingIn,
+    MovieTrending,
+    RecommendationResponse,
+)
