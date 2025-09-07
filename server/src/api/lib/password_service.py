d3841f747387ea2aef0551b162d89cb2a73fec4a
0xdaksh-12 <0xdaksh.12@gmail.com>
2025-09-07 09:35:49 +0530

    feat: add password_service.py

---
+++ server/src/api/lib/password_service.py
@@ -0,0 +1,19 @@
+import bcrypt
+
+
+class PasswordService:
+    """Handle password hashing and verification"""
+
+    @staticmethod
+    def hashed(password: str) -> str:
+
+        return bcrypt.hashpw(
+            password=password[:72].encode("utf-8"), salt=bcrypt.gensalt()
+        ).decode("utf-8")
+
+    @staticmethod
+    def compareHash(password: str, hashed: str) -> bool:
+        return bcrypt.checkpw(
+            password=password[:72].encode("utf-8"),
+            hashed_password=hashed.encode("utf-8"),
+        )
