cdd7d9a19f5ac2fe20bae65421cb6ac8d26feacc
0xdaksh-12 <0xdaksh.12@gmail.com>
2025-09-28 13:52:28 +0530

    feat: add index.ts

---
+++ client/src/types/index.ts
@@ -0,0 +1,54 @@
+export interface User {
+  name: string;
+  profilePic?: string | null;
+}
+
+export interface AuthContextType {
+  user: User | null;
+  isAuth: boolean;
+  loading: boolean;
+  login: (credentials: LoginRequest) => Promise<AuthResponse>;
+  register: (data: RegisterRequest) => Promise<AuthResponse>;
+  logout: () => Promise<void>;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface RegisterRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface AuthResponse {
+  success: boolean;
+  accessToken: string;
+}
+
+export interface Movie {
+  id: number;
+  original_title: string;
+  overview: string;
+  poster_path: string;
+  avg_rating: number;
+}
+
+export interface MovieTrending extends Movie {
+  genres: string[];
+  year: number;
+}
+
+export interface MovieDetail extends MovieTrending {
+  actors: string[];
+  directors: string[];
+  user_rating: number | null;
+}
+
+export type RecommendationResponse = {
+  unlocked: boolean;
+  count?: number;
+  recommendations?: Movie[];
+};
