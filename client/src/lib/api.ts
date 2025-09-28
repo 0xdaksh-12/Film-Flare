2b1ecfa6c6d62f0714e06b11928013c196b36180
0xdaksh-12 <0xdaksh.12@gmail.com>
2025-09-28 12:06:12 +0530

    feat: add api.ts

---
+++ client/src/lib/api.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const apiAuth = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export { apiAuth, api };
