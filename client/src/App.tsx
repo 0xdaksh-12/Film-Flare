8e8f9d0a40fc5e4e95bafafcac1cb3b62192ddf0
0xdaksh-12 <0xdaksh.12@gmail.com>
2025-10-14 13:19:31 +0530

    feat: main application component with routing and providers

---
+++ client/src/App.tsx
@@ -0,0 +1,48 @@
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "sonner";
+import Routes from "./routes";
+import AuthProvider from "./provider/authProvider";
+
+function App() {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
+      <AuthProvider>
+        <Toaster position="bottom-right" />
+        <Routes />
+      </AuthProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
